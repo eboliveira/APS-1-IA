ae93dc9e6d1c652b0b8332585e704108d8bfca55
Igor Faustino <igornfaustino@gmail.com>
2018-05-24 08:36:14 -0300

    add comentarios

--- Codigos/genetico.py
+++ Codigos/genetico.py
@@ -80,94 +80,124 @@ def crossover_alternativo(p1, p2):
     return filho
 
 
-def mutacao(x):
+def mutacao(x, id):
 
-    rand = random.random()
-    if rand >= 0.5:
+    if id == 1:
         # mutacao 1
+        # troca duas posicoes de lugar
+
         index = random.sample(range(0, len(x)), 2)
         aux = x[index[0]]
         x[index[0]] = x[index[1]]
         x[index[1]] = aux
     else:
-        #mutacao 2
-        index = random.randrange(0,len(x))
-        aux = index     #index do vet a ser trocado
-        if x[index] %2 == 0:
-            while x[aux%len(x)] %2 != 0:
-                aux+=1
+        # mutacao 2
+
+        # pega uma posicao aleatoria
+        index = random.randrange(0, len(x))
+        aux = index  # index do vet a ser trocado
+        
+        if x[index] % 2 == 0:
+            while x[aux % len(x)] % 2 != 0:
+                aux += 1
             aux2 = x[aux]
             x[aux] = x[index]
             x[index] = aux2
         else:
-            while x[aux%len(x)] %2 == 0:
-                aux+=1
+            while x[aux % len(x)] % 2 == 0:
+                aux += 1
             aux2 = x[aux]
             x[aux] = x[index]
             x[index] = aux2
     return x
 
 
-
-
-def genetico(pop_inicial, f, n_iter, tx_mutacao, matriz, crossover_alternativo=False, elitismo=False):
+def genetico(pop_inicial, f, n_iter, tx_mutacao, matriz, crossover_alternativo=False, id_mutacao=1, elitismo=False):
     pop = pop_inicial
+    
+    # calcula o fit da pop inicial
     fit = map((lambda x: f(x, matriz)), pop)
 
+    # armazena a melhor solucao ate entao
     fit_melhor_caminho = min(fit)
-
     melhor_caminho = pop[fit.index(fit_melhor_caminho)]
 
+    # inicia o contador de geracoes sem mudancas
     n_maximo_sem_mudancas = 0
-    n = 0
+
+    # sera processado geracoes ate ocorrer um caso onde nao exista
+    # mais melhorias por x geracoes
     while n_maximo_sem_mudancas < n_iter:
         p_nova = []
-        print n_maximo_sem_mudancas
+        print(n_maximo_sem_mudancas)
+
+        # para cada individuo na populacao
         for i in range(len(pop_inicial)):
-            n += 1
-            # print(n)
+            # escolhe dois pais
             x = random_select(pop, f, matriz)
             y = random_select(pop, f, matriz)
 
+            # realiza a reproducao
             if crossover_alternativo:
                 novo = crossover_alternativo(x, y)
             else:
                 novo = crossover(x, y)
 
+            # realiza a mutacao
             r = random.randrange(0, 100)
             if r < tx_mutacao:
-                mutacao(novo)
+                mutacao(novo, id_mutacao)
+            
+            # insere filho na nova pop
             p_nova.append(novo)
 
         if elitismo:
-
+            # ordena uma copia do fit da populacao
+            # desta forma o vertor ficara com ordenado do
+            # pior para o melhor valor
             pop_sort = copy.copy(fit)
-            pop_sort.sort(reverse=True)
+            pop_sort.sort(reverse=True)  # maior para o menor
 
+            # cria um vetor com o fitness dos filhos
             filhos = []
             for i in p_nova:
                 filhos.append(f(i, matriz))
 
+            # para cara fit da pop, verificar se existe um
+            # fit melhor na nova pop
             for pai in pop_sort:
                 menor = float('inf')
                 for filho in filhos:
                     if pai > filho:
                         if (p_nova[filhos.index(filho)] not in pop):
                             if filho < menor:
+                                # Menor e o melhor filho que consegue
+                                # substituir o pai
+                                # e que ainda nao tenha sido inserido no vetor
                                 menor = filho
                 if menor != float('inf'):
+                    # Por fim, o pai e substituido pelo melor filho
                     pop[fit.index(pai)] = p_nova[filhos.index(menor)]
+                # caso o pai seja melhor que os filhos, o mesmo e mantido
+                # para a proxima geracao
         else:
+            # caso nao exista elitismo...
+            # a nova populacao substitui a pop original
             pop = p_nova
+
+        # calcular a fit da nova geracao
         fit = map((lambda x: f(x, matriz)), pop)
 
+        # caso alguma fit desta nova populacao seja a melhor ja vista
         if min(fit) < fit_melhor_caminho:
+            # atualiza a melhor fit, melhor camino
+            # e zera o contador de geracoes sem mudancas
             fit_melhor_caminho = min(fit)
             melhor_caminho = pop[fit.index(fit_melhor_caminho)]
             n_maximo_sem_mudancas = 0
         else:
+            # caso contrario, aumenta o contador de geracoes sem mudancas
             n_maximo_sem_mudancas += 1
-
 
     return fit_melhor_caminho, melhor_caminho
 
--- Codigos/main.py
+++ Codigos/main.py
@@ -5,9 +5,17 @@ import genetico
 
 def main(argv):
     file = open(argv[1], 'r')
-    coordenadas, header = utils.formatar_coord(file)        #formata as coordenadas numa matriz, retorna coordenadas e o cabecalho do arquivo
-    matriz = utils.gerar_matriz(header, coordenadas)        #gera a matriz de distancias entre os pontos
-    pop = genetico.gerar_populacao(10, int(header['DIMENSION']))
+    # formata as coordenadas numa matriz, retorna coordenadas e
+    # o cabecalho do arquivo
+    coordenadas, header = utils.formatar_coord(file)
+
+    # gera a matriz de distancias entre os pontos
+    matriz = utils.gerar_matriz(header, coordenadas)
+
+    # gera uma populacao inicial
+    pop = genetico.gerar_populacao(100, int(header['DIMENSION']))
+
+
     resultados = genetico.genetico(
         pop, genetico.fitness, 100, 5, matriz, elitismo=True)
     for resultado in resultados:
